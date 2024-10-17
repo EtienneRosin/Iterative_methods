6e24bccbb5cde6b8332d28b44ac899150d360cb0
erosin <erosin69@gmail.com>
2024-10-17 17:09:47 +0200

    add choleski sbatch

---
+++ jac_weak_chol.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=Jacobi_sequential_weak_scalability_measurement
+#SBATCH --output=output_%j.txt
+#SBATCH --error=error_%j.txt
+#SBATCH --partition=cpu_test
+#SBATCH --account=ams301
+#SBATCH --ntasks=5
+#SBATCH --time=00:10:00
+## load modules
+module load cmake
+module load gcc
+module load gmsh
+module load openmpi
+## execution
+mpirun -display-map ${SLURM_SUBMIT_DIR}/2_jacobi_weak_scalability
